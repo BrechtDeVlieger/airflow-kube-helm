b52cb0336d66bf7ae1d28032f50cab85023a9749
gracejansen227 <graceowensjansen@gmail.com>
2020-03-12 17:41:59 -0500

    Add python dependencies

--- examples/minikube/docker/build-docker.sh
+++ examples/minikube/docker/build-docker.sh
@@ -20,7 +20,7 @@ cp $DIR/../../../config/airflow.cfg docker-airflow/config/airflow.cfg
 cp $DIR/Dockerfile docker-airflow/Dockerfile
 cd docker-airflow
 
-docker build --build-arg PYTHON_DEPS="Flask-OAuthlib" --build-arg AIRFLOW_DEPS="kubernetes" --tag=${IMAGE}:${TAG} .
+docker build --build-arg PYTHON_DEPS="Flask-OAuthlib psycopg2 psycopg2-binary" --build-arg AIRFLOW_DEPS="kubernetes" --tag=${IMAGE}:${TAG} .
 
 cd ../..
 rm -rf .tmp/
